2dd150fc9e8b6ce69b1da07aec2509d7880a6f90
eilon299 <eilonkornboim@gmail.com>
2021-01-22 17:13:56 +0000

    wip

--- wet/main.c
+++ wet/main.c
@@ -2,23 +2,20 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <syscall.h>
+#include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/reg.h>
+#include <sys/user.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/ptrace.h>
-#include <sys/reg.h>
-#include <sys/user.h>
-#include <syscall.h>
-#include <sys/syscall.h>
 #include <fcntl.h>
 #include <stdbool.h>
 
 
-
-
-pid_t run_target(char** argv) {
+pid_t run_target(char *const argv[]) {
     pid_t pid;
 
     pid = fork();
@@ -30,6 +27,7 @@ pid_t run_target(char** argv) {
             exit(1);
         }
         if (execv(argv[0], argv) < 0) {
+
             perror("execv");
             exit(1);
         }
@@ -38,15 +36,9 @@ pid_t run_target(char** argv) {
         perror("fork");
         exit(1);
     }
+    return -1;
 }
 
-void restore_command_that_had_breakpoint_on_it(pid_t program_pid, struct user_regs_struct* regs,
-                                               unsigned long addr, unsigned long data){
-    ptrace(PTRACE_GETREGS, program_pid, 0, regs);
-    ptrace(PTRACE_POKETEXT, program_pid, (void *) addr, (void *) data);
-    regs->rip -= 1;
-    ptrace(PTRACE_SETREGS, program_pid, 0, &regs);
-}
 
 void p_trace_syscall_and_wait(int* wait_status, pid_t program_pid){
     if(ptrace(PTRACE_SYSCALL, program_pid, NULL, NULL) < 0){
@@ -85,6 +77,31 @@ void set_regs(pid_t program_pid, struct user_regs_struct* regs){
         exit(1);
     }
 }
+void poke_text(pid_t program_pid, unsigned long addr, unsigned long data) {
+    if (ptrace(PTRACE_POKETEXT, program_pid, (void *) addr, (void *) data) < 0){
+        perror("ptrace poke text");
+        exit(1);
+    }
+}
+
+unsigned long peek_text(pid_t program_pid, unsigned long addr){
+    unsigned long data = ptrace(PTRACE_PEEKTEXT, program_pid, (void *) addr, NULL);
+    if (data < 0){
+        perror("ptrace poke text");
+        exit(1);
+    }
+    return data;
+}
+
+void restore_command_that_had_breakpoint_on_it(pid_t program_pid, struct user_regs_struct* regs,
+                                               unsigned long addr, unsigned long data){
+    get_regs(program_pid, regs);
+    poke_text(program_pid, addr, data);
+    regs->rip -= 1;
+    ptrace(PTRACE_SETREGS, program_pid, 0, &regs);
+}
+
+
 
 void holder_func(int* wait_status, bool copy, int fd, pid_t program_pid,
                  struct user_regs_struct* regs) {
@@ -115,32 +132,40 @@ void holder_func(int* wait_status, bool copy, int fd, pid_t program_pid,
     get_regs(program_pid, regs);
 }
 
-void debug(unsigned long addr, char* flag, pid_t program_pid, int fd){
+void debug(unsigned long addr, bool is_the_flag_c, pid_t program_pid, int fd){
 
     int wait_status;
     wait(&wait_status);
 
-    struct user_regs_struct regs;
-    struct user_regs_struct old_regs;
-    unsigned long return_data, data;
-    unsigned long return_data_trap, data_trap;
+    struct user_regs_struct regs, old_regs;
+    unsigned long return_data, data, return_data_trap, data_trap;
 
     //write int 3 to the address
-    data = ptrace(PTRACE_PEEKTEXT, program_pid, (void *) addr, NULL);
-    data_trap = (data & 0xFFFFFF00) | 0xCC;
-    ptrace(PTRACE_POKETEXT, program_pid, (void *) (addr), (void *) (data_trap));
-//    FILE* fp = fopen(output_file_name, "w");
-// ----------------------------------------------passed from main
-//    int fd = fileno(fp);
-
     if(!WIFSTOPPED(wait_status)){
-        int x;
+        puts("problem before loop");
+        return;
     }
-    while (1){
+
+    data = peek_text(program_pid, addr);
+    data_trap = (data & 0xFFFFFFFFFFFFFF00) | 0xCC;
+    poke_text(program_pid, addr, data_trap);
+
+    printf("data = %lx, trap = %lx\n", data, data_trap);
+
+    while (!WIFEXITED(wait_status)){
+        get_regs(program_pid, &regs);
+//        printf("child starting rip = %llx\n", regs.rip);
         p_trace_cont_and_wait(&wait_status, program_pid);
+        get_regs(program_pid, &regs);
+//        long cmnd = peek_text(program_pid, regs.rip-1);
+//        printf("child stopped at rip = %llx\n, and the command is %lx\n", regs.rip, cmnd);
+
         if(!WIFSTOPPED(wait_status)){ //check if the program did not stop because of the breakpoint but for a different reason
+            puts("WIFSTOPPED breaked in start of loop");
             break;
         }
+        printf("rip = %llx\n", regs.rip);
+
         restore_command_that_had_breakpoint_on_it(program_pid, &regs, addr, data);
         p_trace_single_step_and_wait(&wait_status, program_pid); //perform single step in order to place the brake point back on the command we fixed
         if(!WIFSTOPPED(wait_status)){
@@ -152,7 +177,7 @@ void debug(unsigned long addr, char* flag, pid_t program_pid, int fd){
         get_regs(program_pid, &regs);
         return_data = ptrace(PTRACE_PEEKTEXT, program_pid, (void *)(regs.rbp+8), NULL);
         //write int3 to the return address
-        return_data_trap = (return_data & 0xFFFFFF00) | 0xCC;
+        return_data_trap = (return_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
         ptrace(PTRACE_POKETEXT, program_pid, (void *)(regs.rbp+8), (void *) (return_data_trap));
 
         while(1){ //find syscalls within the func
@@ -179,7 +204,7 @@ void debug(unsigned long addr, char* flag, pid_t program_pid, int fd){
             regs.rdi = fd;
             ptrace(PTRACE_SETREGS, program_pid, 0, &regs);
             puts("before holder func");
-            holder_func(&wait_status, flag, program_pid, &old_regs);
+//            holder_func(&wait_status, is_the_flag_c, program_pid, &old_regs);
         }
     }
     if (WIFEXITED(wait_status)) {
@@ -191,13 +216,12 @@ void debug(unsigned long addr, char* flag, pid_t program_pid, int fd){
 
 }
 
+
 int main(int argc, char** argv) {
     // Read arguments
     unsigned long addr = strtoul(argv[1], NULL, 16);
     char* flag = argv[2];
     char* output_file_name = argv[3];
-
-    pid_t program_pid;
 
     int fd = open(output_file_name, O_CREAT|O_WRONLY|O_TRUNC, 0644);
     if (fd < 0){
@@ -205,8 +229,10 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    program_pid = run_target(argv + 4);
-    debug(addr, flag, program_pid, fd);
+
+    pid_t program_pid = run_target(argv + 4);
+    bool is_the_flag_c = *flag == 'c';
+    debug(addr, is_the_flag_c, program_pid, fd);
 
     if (close(fd) < 0) {
         perror("close");
